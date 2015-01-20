472ba04c49d62e859c70819e6a6d20823c946ea5
Rob Ellis <rob@chloi.io>
2015-01-20 13:06:29 -0800

    Bump version

--- test/basic.js
+++ test/basic.js
@@ -1,17 +1,37 @@
-var debug = require('debug')("Server");
-var data = ['./test/data/concepts.top'];
-
 var facts = require("../");
+var sfacts = facts.create("systemDB");
+var userA = sfacts.createUserDB("userA").db;
+var userB = sfacts.createUserDB("userB").db;
 
-var f = facts.explore('testfacts')
-f.conceptToList("family_members", function(e,r) {
-  console.log("--", r);
+// sfacts.db.put({subject:"global", predicate:"name", object : "user" })
+
+// var triple1 = {subject:"my", predicate:"name", object : "Rob" };
+// var triple2 = {subject:"my", predicate:"name", object : "Jane" };
+
+// userA.put(triple1, function(){
+// 	userB.put(triple2, function(){
+// 		console.log("Done");
+// 		process.exit();
+// 	});
+// });
+
+
+var gtriple = {subject:"my", predicate:"name"};
+
+// SOme time later
+userA.get(gtriple, function(e,r){
+	console.log(e,r);
+	userB.get(gtriple, function(e,r){
+		console.log(e,r);
+		console.log("Done");
+
+		sfacts.db.get({}, function(e,r){
+			console.log(r);
+			process.exit();
+		})
+		
+	});
 });
 
 
-// facts.load(data, 'testfacts', function(err, f) {  
-//   f.conceptToList("family_members", function(e,r) {
-//     console.log("--", e,r);
-//   });
-// });
 
