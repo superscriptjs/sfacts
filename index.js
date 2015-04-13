261e1fa0bca3066b051a7bb7e1cdfc3a6fafad23
Rob Ellis <rob@chloi.io>
2015-04-12 23:27:36 -0700

    More hinting

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ exports.explore = function(dbname) {
 	var db = require('levelgraph-recursive')(levelgraph(level));
 
 	return require("./lib/expand")(db, level);
-}
+};
 
-exports.load = require("./lib/system").load; 
-exports.create = require("./lib/system").create; 
+exports.load = require("./lib/system").load;
+exports.create = require("./lib/system").create;
 exports.db = require("./lib/system").db;
